29e684b23db8aee2201a45449036b9eb318cf22a
jishmitha-26 <jishmithamalluvalasa@gmail.com>
2026-07-05 20:57:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,11 @@ TRAIN_PATH = os.path.join(BASE_DIR, "..", "data", "train.csv")
 # ---------------------------------------------------
 # LOAD MODEL
 # ---------------------------------------------------
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "xgboost_sales_model.pkl")
 
 model = joblib.load(MODEL_PATH)
 
